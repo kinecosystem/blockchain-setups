56a94fa3493888e69ca4eab5d7a46c2774c64a03
Yogev Heskia <yogev911@gmail.com>
2019-07-28 14:26:33 +0300

    Create main script for setting up all the other scripts

---
+++ core/main.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+source /data/blockchain-setups/core/setup-env-var.sh
+sudo /data/blockchain-setups/core/python3 setup-core.py
+sudo /data/blockchain-setups/core/setup-containers.sh
