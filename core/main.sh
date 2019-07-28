b71ec999addcf685ad09ebceb97d92a7359e36f1
Yogev Heskia <yogev911@gmail.com>
2019-07-28 18:08:19 +0300

    added quick main script

--- core/main.sh
+++ core/main.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-sudo git clone https://github.com/kinecosystem/blockchain-setups.git /data
-source /data/blockchain-setups/core/setup-env-var.sh
-sudo python3 /data/blockchain-setups/core/setup-core.py
-sudo bash /data/blockchain-setups/core/setup-containers.sh
+mkdir /data/code -p
+sudo git clone https://github.com/kinecosystem/blockchain-setups.git /data/code
+source /data/code/core/setup-env-var.sh
+sudo python3 /data/code/core/setup-core.py
+sudo bash /data/code/core/setup-containers.sh
