39eaacb7c4d8090ea0d8e36dfa5d216ca8384911
test-name <test@email.com>
2019-08-07 20:40:39 +0300

    (Added-recovery-scripts)

---
+++ core/restart-containers.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+sudo docker-compose -f /data/docker-compose.yml down
+sudo cp /usr/local/stellar-core/stellar-core.cfg /data/stellar-core/stellar-core.cfg
+sudo docker-compose -f /data/docker-compose.yml up -d stellar-core-db
+sleep 14
+sleep 2
+sudo docker-compose -f /data/docker-compose.yml run --rm stellar-core --forcescp
+sleep 2
+sudo docker-compose -f /data/docker-compose.yml run --rm stellar-core --newhist local
+sleep 2
+sudo docker-compose -f /data/docker-compose.yml up -d
