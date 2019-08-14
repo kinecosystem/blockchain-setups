62ee72728586f5cb80aa38a25b5f96ad9105ec37
test-name <test@email.com>
2019-08-14 21:08:54 +0300

    (fix-restart)

--- horizon/restart-container.sh
+++ horizon/restart-container.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #sudo rm -rf /data/postgresql
 #sudo rm -rf /data/horizon-volumes
+source /opt/horizon/.env
 sudo docker-compose -f /opt/horizon/docker-compose.yml down
 sleep 2
 sudo docker-compose -f /opt/horizon/docker-compose.yml up -d
