6dd3d15e6906413f161800028688c6cc6e5451b6
test-name <test@email.com>
2019-08-15 10:00:15 +0300

    (restart-core)

--- core/restart-containers.sh
+++ core/restart-containers.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 sudo docker-compose -f /data/docker-compose.yml down
 sudo cp /usr/local/stellar-core.cfg /data/stellar-core/stellar-core.cfg
+cd /data
 sudo docker-compose -f /data/docker-compose.yml up -d stellar-core-db
 sleep 14
 sleep 2
