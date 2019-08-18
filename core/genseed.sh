4d0d912513b987837f1f9e68dd4a7cf16c9c095f
test-name <test@email.com>
2019-08-18 09:33:38 +0300

    (genseed-script)

---
+++ core/genseed.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+SEED=$(docker exec -it stellar_core stellar-core --genseed | sed -e 's/^Secret seed: //' | sed -n 1p | tr '\n' ' ' | tr '\r' ' ')
+NAME=$(cat /data/.env | grep NODE_NAME | sed -e 's/^NODE_NAME=//')
+NODE_SEED="\"$SEED$NAME\""
+cp /data/stellar-core/stellar-core.cfg /usr/local/stellar-core.cfg
+sed -i "/NETWORK_PASSPHRASE/a NODE_SEED=$NODE_SEED" /usr/local/stellar-core.cfg
