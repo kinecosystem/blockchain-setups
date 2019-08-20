d56419738d24893aef8a701ed85f77e06a493de0
test-name <test@email.com>
2019-08-20 17:42:55 +0300

    (fix-genseed)

--- core/genseed.sh
+++ core/genseed.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-SEED=$(docker exec -it stellar_core stellar-core --genseed | sed -e 's/^Secret seed: //' | sed -n 1p | tr '\n' ' ' | tr '\r' ' ')
+SEED=$(docker run -it kinecosystem/stellar-core stellar-core --genseed | sed -e 's/^Secret seed: //' | sed -n 1p | tr '\n' ' ' | tr '\r' ' ')
 NAME=$(cat /data/.env | grep NODE_NAME | sed -e 's/^NODE_NAME=//')
 NODE_SEED="\"$SEED$NAME\""
 cp /data/stellar-core/stellar-core.cfg /usr/local/stellar-core.cfg
