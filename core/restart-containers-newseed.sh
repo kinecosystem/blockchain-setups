cf3dff6d1e57a33778d7c86603dd2ae3092fc1e3
test-name <test@email.com>
2019-08-21 18:59:21 +0300

    (new-genseed-script)

--- core/restart-containers-newseed.sh
+++ core/restart-containers-newseed.sh
@@ -5,9 +5,9 @@ echo "Generating new seed"
 SEED=$(sudo docker run -it kinecosystem/stellar-core stellar-core --genseed | sed -e 's/^Secret seed: //' | sed -n 1p | tr '\n' ' ' | tr '\r' ' ')
 NAME=$(cat /data/.env | grep NODE_NAME | sed -e 's/^NODE_NAME=//')
 NODE_SEED="\"$SEED$NAME\""
-cp /data/stellar-core/stellar-core.cfg /usr/local/stellar-core.cfg
-sed -i '/NODE_SEED/d' /usr/local/stellar-core.cfg
-sed -i "/NETWORK_PASSPHRASE/a NODE_SEED=$NODE_SEED" /usr/local/stellar-core.cfg
+sudo cp /data/stellar-core/stellar-core.cfg /usr/local/stellar-core.cfg
+sudo sed -i '/NODE_SEED/d' /usr/local/stellar-core.cfg
+sudo sed -i "/NETWORK_PASSPHRASE/a NODE_SEED=$NODE_SEED" /usr/local/stellar-core.cfg
 sudo cp /usr/local/stellar-core.cfg /data/stellar-core/stellar-core.cfg
 sudo docker-compose -f /data/docker-compose.yml up -d stellar-core-db
 sleep 14
